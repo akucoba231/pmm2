7d87fd7f65ba1f2a2b80383d65594d2a00d26ad7
akucoba231 <aku.coba231@gmail.com>
2025-09-17 20:34:56 +0700

    Update export.js

--- export.js
+++ export.js
@@ -27,17 +27,10 @@ function showAllRecord(){
 
     exportXLS.onclick = ()=>{
         if(localStorage.getItem('dataNilai') == null){
-            alert('Data tidak ditemukan, atau sudah diihapus');
+            alert('Data tidak ditemukan, atau sudah dihapus');
         }
         else {
-            if(confirm('Apakah Anda ingin menghapus juga semua data penilaian ?'))
-            {
-                localStorage.removeItem('dataNilai');
-            }
-            else {
-
-            }
-            exportToXLS();
+            konfirmasi('Apakah Anda ingin menghapus juga semua data penilaian ?')            
         }
     }
 
@@ -81,4 +74,40 @@ function showAllRecord(){
 let tombolShowRecord = document.getElementById('show-all');
 tombolShowRecord.onclick = ()=>{
     showAllRecord();
+}
+
+function konfirmasi(str){
+    let wrapper = document.createElement('div');
+    let pesan = document.createElement('div');
+    let isiPesan = document.createElement('span');
+    let wrapperTombol = document.createElement('div');
+    let iya = document.createElement('button');
+    let tidak = document.createElement('button');
+
+    iya.textContent = "iya";
+    iya.setAttribute('style','text-align: center; padding: 2% 3%; border-radius: 8px; background: #6D94C5; width: 150px; border: 1px solid white;')
+    tidak.textContent = "tidak";
+    tidak.setAttribute('style','text-align: center; padding: 2% 3%; border-radius: 8px; background: #37353E; width: 150px; border: 1px solid white;')
+
+    wrapperTombol.appendChild(iya);
+    wrapperTombol.appendChild(tidak);
+    wrapperTombol.setAttribute('style','display: flex; justify-content: center; align-items: center; gap: 5px; flex-wrap: nowrap; color: white;')
+
+    isiPesan.textContent = str;
+    pesan.appendChild(isiPesan);
+    pesan.appendChild(wrapperTombol);
+    pesan.setAttribute('style','display: flex; gap: 10px; flex-direction: column; justify-content: center; align-items: center; padding: 3% 5%; background: rgba(50,50,50,.9); color: white; border-radius: 8px; width: 480px;');
+    wrapper.appendChild(pesan);
+    wrapper.setAttribute('style','display: flex; justify-content: center; align-items: center; position: fixed; top: 0; left: 0; width: 100vw; height: 100vh; z-index: 15; background: rgba(0,0,0,.5);');
+    document.body.appendChild(wrapper);
+
+    iya.onclick = () => {
+        document.body.removeChild(wrapper);
+        localStorage.removeItem('dataNilai');
+        exportToXLS();
+    }    
+    tidak.onclick = () => {
+        document.body.removeChild(wrapper);
+        exportToXLS();
+    }
 }
